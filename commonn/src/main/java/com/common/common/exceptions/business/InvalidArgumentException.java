5d30a15388f647d49ffd8c7af56aeaeb07326a1b
Ghita <bahajghita3@gmail.com>
2023-06-12 11:20:22 +0100

    start working

---
+++ commonn/src/main/java/com/common/common/exceptions/business/InvalidArgumentException.java
@@ -0,0 +1,13 @@
+package com.common.common.exceptions.business;
+
+public class InvalidArgumentException extends BusinessException {
+    private static final long serialVersionUID = 7830064295968918115L;
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+    public InvalidArgumentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ commonn/src/main/java/com/common/common/exceptions/response/ValidationResponse.java
@@ -0,0 +1,24 @@
+package com.common.common.exceptions.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+public class ValidationResponse {
+
+    private String object;
+
+    private String field;
+
+    private Object rejectedValue;
+
+    private String message;
+
+    ValidationResponse(String object, String message) {
+        this.object = object;
+        this.message = message;
+    }
+}
